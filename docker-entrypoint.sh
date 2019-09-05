2d210a94f46a138d2b2156f2a0f49b95d6cde786
hdszju <hdszju@163.com>
2019-09-05 09:41:55 +0800

    master node
    master节点

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,9 @@
 set -x
 cd /phxsql/tools
 python2.7 install.py -i "$(hostname -I)" -p 54321 -g 6000 -y 11111 -P 17000 -a 8001 -f /data "$@"
+
+
+
+./phxbinlogsvr_tools_phxrpc -f InitBinlogSvrMaster -h"$,$IP2,$IP3" -p 17000
+
 exec tail -f /dev/null
