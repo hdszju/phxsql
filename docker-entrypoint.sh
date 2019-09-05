434b3ea734965b4a9358da6cf5ac7b2274d8a44e
hdszju <hdszju@163.com>
2019-09-05 09:52:04 +0800

    test
    增加时间延迟

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 cd /phxsql/tools
 python2.7 install.py -i "$(hostname -I)" -p 54321 -g 6000 -y 11111 -P 17000 -a 8001 -f /data "$@"
 
-
+sleep 100
 
 ./phxbinlogsvr_tools_phxrpc -f InitBinlogSvrMaster -h"$,$IP2,$IP3" -p 17000
 
