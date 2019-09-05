13f04239508768f8257d234f09de09c8f077fe4f
hdszju <hdszju@163.com>
2019-09-05 09:56:43 +0800

    test
    修改bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ python2.7 install.py -i "$(hostname -I)" -p 54321 -g 6000 -y 11111 -P 17000 -a 8
 
 sleep 100
 
-./phxbinlogsvr_tools_phxrpc -f InitBinlogSvrMaster -h"$,$IP2,$IP3" -p 17000
+./phxbinlogsvr_tools_phxrpc -f InitBinlogSvrMaster -h"$IP1,$IP2,$IP3" -p 17000
 
 exec tail -f /dev/null
